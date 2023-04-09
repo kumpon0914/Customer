00ef32e0de473d8b097df0194da33ab6034c7f79
kumpon0914 <61109015+kumpon0914@users.noreply.github.com>
2023-04-09 14:34:34 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -21,8 +21,8 @@ dt = pd.read_csv("./Data/Customer.csv")
 st.write(dt.head(100))
 
 dt1 = dt['Age'].sum()
-dt2 = dt['Annual_Income'].sum()
-dt3 = dt['Spending_Score'].sum()
+dt2 = dt['Annual_Income ($)'].sum()
+dt3 = dt['Spending Score (1-100)'].sum()
 dt4 = dt['Work_Experience'].sum()
 dt5 = dt['Family_Size'].sum()
 
@@ -32,5 +32,41 @@ if st.button("แสดงการจินตทัศน์ข้อมูล
    #st.write(dt.head(10))
    st.bar_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
+else:
+   st.write("ไม่แสดงข้อมูล")
+
+html_8 = """
+<div style="background-color:#D5DBDB;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h4>ทำนายข้อมูล</h4></center>
+</div>
+"""
+
+pt_len=st.slider("กรุณาเลือกข้อมูล Age")
+pt_wd=st.slider("กรุณาเลือกข้อมูล Annual_Income")
+sp_len=st.number_input("กรุณาเลือกข้อมูล Spending_Score")
+sp_wd=st.number_input("กรุณาเลือกข้อมูล Work_Experience")
+sp_wd=st.number_input("กรุณาเลือกข้อมูล Family_Size")
+
+if st.button("ทำนายผล"):
+   
+   X = dt.drop('variety', axis=1)
+   y = dt.variety
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./Pic/Set1.jpg", use_column_width=True)
+   elif out[0] == 'Virginica':
+    st.image("./Pic/Vir.jpg", use_column_width=True)
+   elif out[0] == 'Versicolor':
+    st.image("./Pic/Col.png", use_column_width=True)
+   else:       
+    st.writ('xxx')    
+   #st.button("ไม่แสดงข้อมูล")
 else:
    st.write("ไม่แสดงข้อมูล")
