8d368b22ee4522845b95f31eebb1360a3cf49cfa
kumpon0914 <61109015+kumpon0914@users.noreply.github.com>
2023-04-10 13:51:24 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -44,6 +44,7 @@ html_8 = """
 </div>
 """
 
+ag=st.slider("กรุณาเลือกข้อมูล Age")
 al_in=st.number_input("กรุณาเลือกข้อมูล Annual Income ($)")
 sp_sc=st.slider("กรุณาเลือกข้อมูล Spending Score (1-100)")
 wk_exp=st.slider("กรุณาเลือกข้อมูล Work Experience")
